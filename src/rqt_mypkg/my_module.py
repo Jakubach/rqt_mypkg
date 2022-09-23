57ba258d51d4a44b772d98d56bcde87e8287c0e0
Jakubach <jakubach@gmail.com>
2022-09-24 01:17:38 +0200

    Update my_module.py

--- src/rqt_mypkg/my_module.py
+++ src/rqt_mypkg/my_module.py
@@ -8,7 +8,7 @@ import cv2 as cv
 
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi, QtGui, QtCore
-from QtCore import Qt,Signal
+from QtCore import Qt
 from python_qt_binding.QtWidgets import QWidget, QPushButton, QLabel
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
